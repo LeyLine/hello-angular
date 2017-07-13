9bc32d44e5b185f3f155e65b9ff4398303bd4ae8
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-07-13 13:03:21 -0400

    Add directive examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { PipesComponent } from './pipes/pipes.component';
 import { CapitalizePipe } from './my-pipes/capitalize.pipe';
 import { EmojifyPipe } from './my-pipes/emojify.pipe';
 import { DoTheSearchPipe } from './my-pipes/do-the-search.pipe';
+import { DirectivesExampleComponent } from './directives-example/directives-example.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { DoTheSearchPipe } from './my-pipes/do-the-search.pipe';
     PipesComponent,
     CapitalizePipe,
     EmojifyPipe,
-    DoTheSearchPipe
+    DoTheSearchPipe,
+    DirectivesExampleComponent
   ],
   imports: [
     BrowserModule,
